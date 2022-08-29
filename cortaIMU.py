ad3ac31b3678e7b380f26b58fd21a24af71089de
davidib19 <david.cordova@ib.edu.ar>
2022-08-29 15:11:15 -0300

    Todos los datos est[an organizados, de IMU tomé los datos que contenían GPS, está pendiente cambiarlo por un promedio de la temperatura.

--- cortaIMU.py
+++ cortaIMU.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import os
 
+Crudos_path = os.path.join(os.path.split(os.getcwd())[0], "Datos")
+Cortado_path = os.path.join(os.getcwd(), "Datos")
 
-tortugometro_path = os.path.join(os.getcwd(), "Datos_tortugometro_crudo")
-
-
-for document in os.listdir(tortugometro_path):
-    df = pd.read_csv(os.path.join(tortugometro_path, document), dtype={'timeGMT':str}, sep=';', usecols=['date', 'timeGMT',
-                                                                                                      'lat', 'lon',
-                                                                                                      'tempIMU_C'])
-    df[df['lat'].notna()].to_csv(os.path.join(os.getcwd(), "Datos_tortugometro", document))
+for campa in os.listdir(Crudos_path):
+    for document in os.listdir(os.path.join(Crudos_path, campa)):
+        if document[-3:] == "csv":
+            docu = os.path.join(Crudos_path, campa, document)
+            with open(docu, 'r') as f:
+                lines = f.readlines()
+                lines[0] = "date;timeGMT;accX;accY;accZ;tempIMU;girX;girY;girZ;tempIMU_C;Tsample;lat;lon\n"
+            with open(docu, 'w') as f:
+                f.writelines(lines)
+            print(docu)
+            df = pd.read_csv(os.path.join(docu), dtype={'timeGMT':str}, sep=';', usecols=['date', 'timeGMT',
+                                                                                          'lat', 'lon',
+                                                                                          'tempIMU_C'], skipinitialspace=True)
+            df[df['lat'].notna()].to_csv(os.path.join(Cortado_path, campa,document))
